049cd145a1d7d5846dd9c73d11ed3c21ddfec733
qqyqqy-1404 <qqy057217@gmail.com>
2025-10-28 12:07:17 +0800

    测试提交

---
+++ .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n;
+
+   cout<<"请输入你要打印的行数："<<endl;
+   cin>>n;
+ for(int i=0;i<n;i++){
+cout<<"*"<<endl;
+
+ }
+
+ 
+system("pause");
+return 0;
+
